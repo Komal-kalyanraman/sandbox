13837908493c09e37cbc45f3ce81070b45526e85
Komal-kalyanraman <komal.kalyanraman@gmail.com>
2024-04-10 12:08:58 +0530

    Add MQTT webcam streaming functionality

--- MQTT_data_stream/cam_pub.py
+++ MQTT_data_stream/cam_pub.py
@@ -1,8 +1,35 @@
-# Python
+import cv2
 import paho.mqtt.client as mqtt
+import base64
 
+# Set up MQTT client
 client = mqtt.Client()
 client.connect("broker.hivemq.com", 1883, 60)
 
-client.publish("test/topic", "Hello, Komal!")
-client.disconnect()
+# Open webcam
+cap = cv2.VideoCapture(0)
+
+count = 0
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Resize the frame
+    frame = cv2.resize(frame, (640, 480))
+
+    if count == 10:
+        # Convert the image to jpg format
+        _, buffer = cv2.imencode('.jpg', frame)
+        jpg_as_text = base64.b64encode(buffer)
+
+        # Publish the frame
+        client.publish("webcam/frame", jpg_as_text)
+
+        count = 0
+    
+    count += 1
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
--- MQTT_data_stream/cam_sub.py
+++ MQTT_data_stream/cam_sub.py
@@ -1,12 +1,22 @@
-# Python
+import cv2
 import paho.mqtt.client as mqtt
+import base64
+import numpy as np
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe("test/topic")
+    client.subscribe("webcam/frame")
 
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
+
+    # Convert bytes to image
+    img = base64.b64decode(msg.payload)
+    npimg = np.frombuffer(img, dtype=np.uint8)
+    source = cv2.imdecode(npimg, 1)
+
+    # Save image
+    cv2.imwrite('./image.jpg', source)
 
 client = mqtt.Client()
 client.on_connect = on_connect
