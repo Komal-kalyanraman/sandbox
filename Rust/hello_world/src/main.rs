d8bfb06cf55ae1747cb4a6d56a71f83cd43e63af
Komal-kalyanraman <komal.kalyanraman@gmail.com>
2024-04-29 13:42:38 +0530

    added memory usage monitor

--- Rust/hello_world/src/main.rs
+++ Rust/hello_world/src/main.rs
@@ -2,23 +2,31 @@ use std::thread;
 use std::time::Duration;
 use sysinfo::{ProcessorExt, System, SystemExt};
 
-fn cpu_utilization() {
+fn cpu_utilization_and_memory() {
     let mut system = System::new_all();
     let mut cpu_utilization_values = Vec::new();
 
     // We sleep 20ms to get the CPU usage.
     thread::sleep(Duration::from_millis(20));
     system.refresh_all();
-    
+
     for (_core, processor) in system.processors().iter().enumerate() {
         cpu_utilization_values.push(processor.cpu_usage());
     }
     let sum: f32 = cpu_utilization_values.iter().sum();
-    let average_cpu_usage = sum / cpu_utilization_values.len() as f32;
+    let average_cpu_usage = format!("{:.2}", sum / cpu_utilization_values.len() as f32);
+
+    // Get total and free memory
+    let total_memory = system.total_memory();
+    let free_memory = system.available_memory();
+    let memory_usage = format!("{:.2}", ((total_memory as f32 - free_memory as f32)/total_memory as f32)* 100.0);
 
     println!("Average CPU usage: {}%", average_cpu_usage);
+    println!("Memory usage: {}%", memory_usage);
 }
 
 fn main() {
-    cpu_utilization();
+    loop {
+        cpu_utilization_and_memory();
+    }
 }
