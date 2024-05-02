55cd209359c12fa1d0a8bf91af184f2b9f1b041a
Komal-kalyanraman <komal.kalyanraman@gmail.com>
2024-05-02 20:54:37 +0530

    server side streaming

--- Rust/grpc/src/server.rs
+++ Rust/grpc/src/server.rs
@@ -7,8 +7,10 @@ use crate::hello::say_server::{Say, SayServer};
 use crate::hello::{SayRequest, SayResponse};
 use futures::stream::Stream;
 use std::pin::Pin;
-use tokio::stream::StreamExt;
-use futures::stream::repeat;
+use futures::stream::repeat_with;
+use rand::Rng;
+use rand::rngs::StdRng;
+use rand::SeedableRng;
 
 // defining a struct for our service
 #[derive(Default)]
@@ -20,9 +22,12 @@ impl Say for MySay {
     type SendStream = Pin<Box<dyn Stream<Item = Result<SayResponse, Status>> + Send + Sync + 'static>>;
 
     async fn send(&self, request: Request<SayRequest>) -> Result<Response<Self::SendStream>, Status> {
-        let message = format!("hello {}", request.get_ref().name);
-
-        let output_stream = repeat(Ok(SayResponse { message: message.clone() }));
+        let mut rng = StdRng::from_entropy();
+        let output_stream = repeat_with(move || {
+            let random_number: u32 = rng.gen();
+            let message = format!("hello {}, random number: {}", request.get_ref().name, random_number);
+            Ok(SayResponse { message: message.clone() })
+        });
 
         Ok(Response::new(Box::pin(output_stream)))
     }
