4c0bf3d978eeea6eee3cdc86d87ca65af218a1e5
Komal-kalyanraman <komal.kalyanraman@gmail.com>
2024-05-03 17:21:53 +0530

    cpu usage calculation

--- Rust/grpc/src/server.rs
+++ Rust/grpc/src/server.rs
@@ -2,15 +2,15 @@ pub mod resource_utilization {
     tonic::include_proto!("resource_utilization");
 }
 
+use std::time::Duration;
+use std::thread;
 use tonic::{transport::Server, Request, Response, Status};
 use resource_utilization::resource_utilization_server::{ResourceUtilization, ResourceUtilizationServer};
 use resource_utilization::{ResourceUtilizationRequest, ResourceUtilizationResponse};
 use futures::stream::Stream;
 use std::pin::Pin;
+use sysinfo::{ProcessorExt, System, SystemExt};
 use futures::stream::repeat_with;
-use rand::Rng;
-use rand::rngs::StdRng;
-use rand::SeedableRng;
 
 // defining a struct for our service
 #[derive(Default)]
@@ -20,12 +20,25 @@ pub struct MyResourceUtilization {}
 #[tonic::async_trait]
 impl ResourceUtilization for MyResourceUtilization {
     type SendStream = Pin<Box<dyn Stream<Item = Result<ResourceUtilizationResponse, Status>> + Send + Sync + 'static>>;
-
+    
     async fn send(&self, request: Request<ResourceUtilizationRequest>) -> Result<Response<Self::SendStream>, Status> {
-        let mut rng = StdRng::from_entropy();
+        let mut system = System::new_all();
+        let mut resource_utilization_values = Vec::new();
+
+        // let mut rng = StdRng::from_entropy();
         let output_stream = repeat_with(move || {
-            let random_number: u32 = rng.gen();
-            Ok(ResourceUtilizationResponse { cpu_resource: random_number.clone() })
+            resource_utilization_values.clear();
+            system.refresh_cpu();
+            thread::sleep(Duration::from_secs(1));
+
+            for (_core, processor) in system.processors().iter().enumerate() {
+                resource_utilization_values.push(processor.cpu_usage());
+                print!("{} ", processor.cpu_usage());
+            }
+            
+            let sum: f32 = resource_utilization_values.iter().sum();
+            let total_cpu_usage = (sum / resource_utilization_values.len() as f32) as u32;
+            Ok(ResourceUtilizationResponse { cpu_resource: total_cpu_usage.clone() })
         });
 
         Ok(Response::new(Box::pin(output_stream)))
