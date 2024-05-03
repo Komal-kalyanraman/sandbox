4c28b6d8634ba945dbe284530e9843b2db996b88
Komal-kalyanraman <komal.kalyanraman@gmail.com>
2024-05-03 18:39:43 +0530

    parsing all 3 parameters

--- Rust/grpc/src/server.rs
+++ Rust/grpc/src/server.rs
@@ -12,35 +12,62 @@ use futures::stream::Stream;
 use std::pin::Pin;
 use sysinfo::{ProcessorExt, System, SystemExt};
 use futures::stream::StreamExt;
+use std::time::SystemTime;
+use chrono::{DateTime, Local};
 
 #[derive(Default)]
 pub struct MyResourceUtilization {
     cpu_usage: Arc<Mutex<u32>>,
+    memory_usage: Arc<Mutex<u32>>,
+    timestamp: Arc<Mutex<String>>,
 }
 
 impl MyResourceUtilization {
     pub fn new() -> Self {
         let cpu_usage = Arc::new(Mutex::new(0));
+        let memory_usage = Arc::new(Mutex::new(0));
+        let timestamp = Arc::new(Mutex::new(String::new()));
+
         let cpu_usage_clone = Arc::clone(&cpu_usage);
+        let memory_usage_clone = Arc::clone(&memory_usage);
+        let timestamp_clone = Arc::clone(&timestamp);
 
         thread::spawn(move || {
             let mut system = System::new_all();
             loop {
                 system.refresh_cpu();
-                thread::sleep(Duration::from_millis(50));
+                system.refresh_memory();
 
                 let mut total_cpu_usage = 0.0;
                 for processor in system.processors() {
                     total_cpu_usage += processor.cpu_usage();
                 }
                 total_cpu_usage /= system.processors().len() as f32;
+                
+                // Get total and free memory
+                let total_memory = system.total_memory();
+                let free_memory = system.available_memory();
+                let memory_usage: u32 = (((total_memory as f32 - free_memory as f32)/total_memory as f32)* 100.0) as u32;
+
+                // Get the current system time
+                let system_time = SystemTime::now();
+                let datetime: DateTime<Local> = DateTime::from(system_time);
+                let system_time_string = datetime.format("%Y-%m-%d %H:%M:%S%.3f").to_string();
+                
+                thread::sleep(Duration::from_millis(50));
 
                 let mut cpu_usage_lock = cpu_usage_clone.lock().unwrap();
                 *cpu_usage_lock = total_cpu_usage as u32;
+
+                let mut memory_usage_lock = memory_usage_clone.lock().unwrap();
+                *memory_usage_lock = memory_usage;
+
+                let mut timestamp_lock = timestamp_clone.lock().unwrap();
+                *timestamp_lock = system_time_string;
             }
         });
 
-        Self { cpu_usage }
+        Self { cpu_usage, memory_usage, timestamp }
     }
 }
 
@@ -50,11 +77,20 @@ impl ResourceUtilization for MyResourceUtilization {
     
     async fn send(&self, _request: Request<ResourceUtilizationRequest>) -> Result<Response<Self::SendStream>, Status> {
         let cpu_usage_clone = Arc::clone(&self.cpu_usage);
+        let memory_usage_clone = Arc::clone(&self.memory_usage);
+        let timestamp_clone = Arc::clone(&self.timestamp);
 
         let output_stream = tokio::time::interval(Duration::from_millis(50))
             .map(move |_| {
                 let cpu_usage_lock = cpu_usage_clone.lock().unwrap();
-                let response = ResourceUtilizationResponse { cpu_resource: *cpu_usage_lock };
+                let memory_usage_lock = memory_usage_clone.lock().unwrap();
+                let timestamp_lock = timestamp_clone.lock().unwrap();
+
+                let response = ResourceUtilizationResponse { 
+                    cpu_resource: *cpu_usage_lock, 
+                    memory_resource: *memory_usage_lock, 
+                    timestamp: timestamp_lock.clone() 
+                };
                 Ok(response)
             });
 
