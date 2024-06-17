d6ccf984ce8d438f0ca8264b022c0436105d0236
Komal-kalyanraman <komal.kalyanraman@gmail.com>
2024-06-17 18:52:07 +0530

    added incremental stress

--- stress/incremental_stress.py
+++ stress/incremental_stress.py
@@ -12,20 +12,23 @@ def get_total_memory_kb():
     return 0
 
 # Calculate memory stress in bytes for a given percentage of total memory
-def calculate_memory_stress_bytes(percentage):
+def calculate_memory_stress_bytes(percentage_increment):
     total_memory_kb = get_total_memory_kb()
     # Calculate the memory to stress in KB
-    memory_stress_kb = total_memory_kb * (percentage / 100)
+    memory_stress_kb = total_memory_kb * (percentage_increment / 100)
     # Convert KB to bytes
     return int(memory_stress_kb) * 1024
 
 # Duration for each stress test
-duration = "2s"
+sleep_duration = 2
+percentage_increment = 1
+test_duration = 60
 
 # Loop from 1% to 20% incrementally for both CPU and memory
-for percentage in range(1, 31):
+for _ in range(1, 21):
     # Calculate memory stress in bytes for the current percentage
-    memory_stress_bytes = int(calculate_memory_stress_bytes(percentage)/(1024*1024))
+    
+    memory_stress_bytes = int(calculate_memory_stress_bytes(percentage_increment)/(1024*1024))
     memory_stress_arg = str(memory_stress_bytes) + "M"  # Stress-ng expects a string like "3200M" or "1024K"
     print(memory_stress_arg)
 
@@ -33,24 +36,16 @@ for percentage in range(1, 31):
     command = [
         "stress-ng",
         "--cpu", "0",
-        "--cpu-load", str(percentage),
+        "--cpu-load", str(percentage_increment),
         "--vm", "1",
-        "--vm-bytes", memory_stress_arg,
-        "--timeout", duration
+        "--vm-bytes", memory_stress_arg
     ]
 
-    # command = [
-    #     "stress-ng",
-    #     "--cpu", "0",
-    #     "--cpu-load", "1",
-    #     "--vm", "1",
-    #     "--vm-bytes", "160M",
-    #     "--timeout", duration
-    # ]
+    time.sleep(sleep_duration)
     
     # Execute the command
     print(f"Executing command: {' '.join(command)}")
-    subprocess.run(command)
-
+    subprocess.Popen(command)
+time.sleep(test_duration)
 command = "pkill", "stress-ng"
 subprocess.run(command)
